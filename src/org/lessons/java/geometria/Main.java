19ffc4b9ddd5e793c32e0a80b7f4af901801e6f6
giuseppesciacca <sciaccagi@gmail.com>
2023-09-15 15:37:08 +0200

    add disegna method

--- src/org/lessons/java/geometria/Main.java
+++ src/org/lessons/java/geometria/Main.java
@@ -4,19 +4,39 @@ import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
-	
+
 		Scanner sc = new Scanner(System.in);
 
 		System.out.print("Base rettangolo n.1? ");
 		int baseRet1 = sc.nextInt();
-
 		System.out.print("Altezza rettangolo n.1? ");
 		int altezzaRet1 = sc.nextInt();
-
+		
 		Rettangolo rettangolo1 = new Rettangolo(baseRet1, altezzaRet1);
+		rettangolo1.disegna();
+		
+		System.out.print("\n*******************************\n");
 
+		System.out.print("Base rettangolo n.2? ");
+		int baseRet2 = sc.nextInt();
+		System.out.print("Altezza rettangolo n.2? ");
+		int altezzaRet2 = sc.nextInt();
+		
+		Rettangolo rettangolo2 = new Rettangolo(baseRet2, altezzaRet2);	
+		rettangolo2.disegna();
+		
+		System.out.print("\n*******************************\n");
+
+		System.out.print("Base rettangolo n.3? ");
+		int baseRet3 = sc.nextInt();
+		System.out.print("Altezza rettangolo n.3? ");
+		int altezzaRet3 = sc.nextInt();
+		
 		sc.close();
-		rettangolo1.calcolaArea();
-		rettangolo1.calcolaPerimetro();
+		
+		Rettangolo rettangolo3 = new Rettangolo(baseRet3, altezzaRet3);
+		rettangolo3.disegna();
+		
+		
 	}
 }
--- src/org/lessons/java/geometria/Rettangolo.java
+++ src/org/lessons/java/geometria/Rettangolo.java
@@ -1,5 +1,7 @@
 package org.lessons.java.geometria;
 
+import java.util.Iterator;
+
 public class Rettangolo {
 	public int base;
 	public int altezza;
@@ -12,12 +14,43 @@ public class Rettangolo {
 
 	public void calcolaArea() {
 
+		System.out.println("Base: " + this.base);
+		System.out.println("Altezza: " + this.altezza);
 		System.out.println("Area= " + (base * altezza));
 	}
 
 	public void calcolaPerimetro() {
 
+		System.out.println("Base: " + this.base);
+		System.out.println("Altezza: " + this.altezza);
 		System.out.println("Perimetro= " + (2 * (base * altezza)));
 	}
 
+	public void disegna() {
+
+		System.out.println("Base: " + this.base);
+		System.out.println("Altezza: " + this.altezza);
+		calcolaPerimetro();
+		calcolaArea();
+
+		String baseDrawed = " ";
+		String whiteSpaces = " ";
+
+		for (int i = 0; i < this.base; i++) {
+			baseDrawed = baseDrawed + "o";
+			whiteSpaces = whiteSpaces + " ";
+		}
+
+		System.out.println(baseDrawed);
+
+		for (int i = 0; i < (this.altezza - 2); i++) {
+			String LeftSideDrawed = "o";
+
+			String RightSideDrawed = whiteSpaces + "o";
+			System.out.println(LeftSideDrawed + RightSideDrawed);
+		}
+
+		System.out.println(baseDrawed);
+
+	}
 }
